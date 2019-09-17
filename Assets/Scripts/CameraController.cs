04c3a425067cb2c377fba20ef924975af5dbb3a0
pmcburney <payton.mcburney@my.simpson.edu>
2019-09-17 10:01:42 -0700

    Initial check-in.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame, after other things are updated
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
